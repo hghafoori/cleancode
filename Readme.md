5bec43146dc57ded78ea4449a4032fddec74bc87
Haroon Ghafoori <hghafoori@xebia.com>
2017-10-13 19:34:26 +0530

    Clean code practices project

---
+++ Readme.md
@@ -0,0 +1,8 @@
+This project contains testing and TDD concepts and code quality tools setup
+
+* Cobetura
+* Checkstyle
+* PMD/CPD
+* FindBug
+* Sonar
+* 
---
+++ src/main/java/com/highafoori/cleancode/Calculation.java
@@ -0,0 +1,14 @@
+package com.highafoori.cleancode;
+/*
+ * @author Umashankar 
+ * http://j2eedev.org
+ * Beginer JUnit Tutorial
+ * */
+public class Calculation {
+	public static int add(int a, int b) {
+			return a + b;
+		}
+		public static int sub(int a, int b) {
+			return a - b;
+		}
+	}
